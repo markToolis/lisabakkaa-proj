18bc9427e4d4f7db6b6d6b273aceafd77f896b49
markToolis <toolismark@gmail.com>
2022-10-12 19:29:20 +0300

    офтлотловы

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
 $('.slider').slick({
   infinite: true,
   slidesToShow: 3,
