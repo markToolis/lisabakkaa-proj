e6bccd7e4508daea773b8af70684cafbffdf352d
markToolis <toolismark@gmail.com>
2022-10-15 23:36:25 +0300

    ясчсчс

--- js/script.js
+++ js/script.js
@@ -1,13 +1,29 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 
-menuBtn.addEventListener('click', function(){
-	menuBtn.classList.toggle('active');
-	menu.classList.toggle('active');
+menuBtn.addEventListener('click', function () {
+  menuBtn.classList.toggle('active');
+  menu.classList.toggle('active');
 })
 
 $('.slider').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1 
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1100,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      }
+    },
+    {
+      breakpoint: 700,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
 });
