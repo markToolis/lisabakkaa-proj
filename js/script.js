8f95fd4451e9ec01607c3a1e3a624f0fb278f260
Mark Toolis <toolismark@gmail.com>
2022-11-04 15:23:59 +0300

    sdfsdfsdf

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let portfilio__btn = document.querySelector('.portfilio__btn');
+let portfolio__box = document.querySelector('.portfolio__box');
+let px = -220
 
 menuBtn.addEventListener('click', function () {
   menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
+})
+
+portfilio__btn.addEventListener('click', function () {
+  
+  portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
+  px = px -360
 })
 
 $('.slider').slick({
