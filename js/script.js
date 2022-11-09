6da6cfa73e19793ab6f4f4eb7a691c4b9ad442f8
Mark Toolis <toolismark@gmail.com>
2022-11-09 21:26:36 +0300

    asfsdf

--- js/script.js
+++ js/script.js
@@ -2,15 +2,38 @@ let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
 let portfilio__btn = document.querySelector('.portfilio__btn');
 let portfolio__box = document.querySelector('.portfolio__box');
-let px = -400
+let portfilio__btn2 = document.querySelector('.portfilio__btn-2');
+let px = 0
+let number = 0
 
 menuBtn.addEventListener('click', function () {
   menu.classList.toggle('active');
   menuBtn.classList.toggle('active');
 })
 
-portfilio__btn.addEventListener('click', function () {
-  
-  portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
-  px = px -400
+portfilio__btn.addEventListener('click', function(){
+  if(number <= 2){ 
+      number = number + 1
+      px = px -400
+      portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
+  }
+  else{
+    number = 0
+    px = 0 
+    portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
+  };
+} )
+portfilio__btn2.addEventListener('click', function(){
+  if(number >0 ){
+    number = number - 1
+    px = px +400
+    portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
+  }
+  else{
+    number = 0
+    px = 0 
+    portfolio__box.style.cssText = `transform: translate(${String(px)}px,0);`;
+  }
 })
+// portfilio__btn2.addEventListener('click', function () {
+// })
